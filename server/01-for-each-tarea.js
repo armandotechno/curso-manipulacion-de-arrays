e7ec236efebd19086b176a00f832dd7a56b3f4ad
Armando Capriles <armandotechno12@gmail.com>
2023-01-07 15:33:39 -0400

    Tarea 1 terminada

---
+++ server/01-for-each-tarea.js
@@ -0,0 +1,7 @@
+const tareas = [
+    { name: 'Sacar la basura', status: 'incompleta' },
+    { name: 'Estudiar', status: 'En progreso' },
+    { name: 'Fregar', status: 'completa' },
+]
+
+tareas.forEach( tarea => console.log(tarea))
