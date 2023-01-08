527a599872d23606346b87e61e130de374ca7b34
Armando Capriles <armandotechno12@gmail.com>
2023-01-07 21:13:19 -0400

    Guardando la tercera clase

--- server/03-map.js
+++ server/03-map.js
@@ -24,3 +24,13 @@ const orders = [
 
   const rta = orders.map( order => order.total)
   console.log("rta", rta);
+
+  const rta3 = orders.map( order => {
+    return {
+        ...order,
+        tax: .19
+    }
+  })
+
+  console.log("rta3", rta3);
+  console.log("original", orders);
