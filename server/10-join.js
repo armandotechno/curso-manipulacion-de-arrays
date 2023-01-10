8281e2ee785bfe389bc0b85b40392b96429e0caa
Armando Capriles <armandotechno12@gmail.com>
2023-01-10 12:38:17 -0400

    Clase con Concat finalizada

---
+++ server/10-join.js
@@ -0,0 +1,9 @@
+const elements = ['Fire', 'Air', 'Water']
+
+const separator =  elements.join('--')
+
+console.log(separator);
+
+const title = 'Armando es un estudiante de Platzi'
+const urlFinal = title.split(' ').join('-').toLowerCase()
+console.log(urlFinal);
